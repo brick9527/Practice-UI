c42ac641d12079482092a192831815af76fd285f
brick9527 <brick9527@foxmail.com>
2021-08-18 21:56:20 +0800

    feat: 添加eslint，添加获取系统配置接口

--- src/components/CodeResult/index.js
+++ src/components/CodeResult/index.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import { connect } from 'react-redux';
 import { Layout } from 'antd';
+import PropTypes from 'prop-types';
 
 import './index.less';
 
 const { Header, Content } = Layout;
 
-function CodeResult(props) {
+function CodeResult (props) {
   return (
     <div className="code-result-container">
       <Header className="code-result-header">运行结果:</Header>
@@ -17,7 +19,7 @@ function CodeResult(props) {
             theme: 'material',
             lineNumbers: true,
             tabSize: 2,
-            readOnly: true,
+            readOnly: true
           }}
         />
       </Content>
@@ -25,8 +27,11 @@ function CodeResult(props) {
   );
 }
 
+CodeResult.propTypes = {
+  codeResult: PropTypes.string,
+};
+
 const mapstateToProps = (state) => {
-  console.log('state = ', state);
   return state;
 };
 
